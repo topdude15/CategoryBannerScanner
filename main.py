0eecf401eacb7ded83bfbb9f4023d9a8e8935722
Trevor Rose <topdude15@gmail.com>
2025-02-27 15:31:43 -0700

    Removed pytesseract dependency

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from PIL import Image, ImageDraw, ImageEnhance
-import pytesseract
 import cv2
 import easyocr
 import numpy as np
